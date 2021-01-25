2391617bf741e333c387f20489567e29ef883339
Corina Belenzo <corina.belenzo@gmail.com>
2021-01-25 17:54:37 +0800

    Included humidity in the weather output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!')
         } else {
-            const {weather_descriptions, temperature, feelslike} = body.current
-            callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out. It feels like ' + feelslike + ' degrees out.')
+            const {weather_descriptions, temperature, feelslike, humidity} = body.current
+            callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out. It feels like ' + feelslike + ' degrees out. The humidity is ' + humidity + '%.')
         }
     })
 }
